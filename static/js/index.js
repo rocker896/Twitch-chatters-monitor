4d24b79a27c10d7e2a2da3df4c2534f565fae3b4
ProMaker <rocker896@gmail.com>
2024-10-23 01:18:09 +0800

    ✨♻️ 現在能關閉自動更新及調整更新時間並重構大部分程式 重構是因為原先方式也會更新工具列而重置下拉式選單，以致不知當前設定的更新間隔為何

--- static/js/index.js
+++ static/js/index.js
@@ -1,100 +1,27 @@
-$(() => {
-    function fetchData() {
-        // 發送請求以獲取頻道的聊天者資料
-        fetch(`/api/chatters/${channel}`)
-            .then((response) => response.json())
-            .then((data) => {
-                // 初始化資料表格
-                $("#gridContainer")
-                    .dxDataGrid({
-                        dataSource: data.chatters, // 設定資料來源
-                        allowColumnReordering: true, // 允許重新排序欄位
-                        width: "100%", // 設定寬度為 100%
-                        height: "100%",
-                        showBorders: true, // 顯示邊框
-                        hoverStateEnabled: true, // 啟用懸停狀態
-                        grouping: {
-                            autoExpandAll: true, // 自動展開所有分組
-                            expandMode: "rowClick", // 點擊行展開
-                        },
-                        searchPanel: {
-                            visible: true, // 顯示搜尋面板
-                        },
-                        paging: {
-                            enabled: false,
-                        },
-                        groupPanel: {
-                            visible: true, // 顯示分組面板
-                        },
-                        selection: {
-                            mode: "single", // 單選模式
-                        },
-                        columns: [
-                            {
-                                dataField: "role", // 角色欄位
-                                caption: "角色", // 欄位標題
-                                groupIndex: 0, // 設定為第一個分組欄位
-                                groupCellTemplate: (element, options) =>
-                                    setCellTemplate(element, options), // 自定義分組單元格模板
-                                cellTemplate: (element, options) =>
-                                    setCellTemplate(element, options), // 自定義單元格模板
-                            },
-                            {
-                                dataField: "name", // 名稱欄位
-                                caption: "名稱", // 欄位標題
-                                allowGrouping: false, // 不允許分組
-                                cellTemplate(container, options) {
-                                    // 自定義單元格模板，生成超連結
-                                    return $("<a>", {
-                                        href: `https://www.twitch.tv/${options.value}`,
-                                        target: "_blank", // 在新標籤頁中打開
-                                    }).text(options.value);
-                                },
-                            },
-                        ],
-                        sortByGroupSummaryInfo: [{ summaryItem: "count" }], // 根據分組摘要資訊排序
-                        summary: {
-                            groupItems: [
-                                { column: "role", summaryType: "count" }, // 計算每個角色的數量
-                            ],
-                            totalItems: [
-                                {
-                                    name: "ChattersCount",
-                                    showInColumn: "name",
-                                    displayFormat: "聊天室人數: {0}",
-                                    summaryType: "custom",
-                                },
-                            ],
-                            calculateCustomSummary(options) {
-                                options.totalValue = data.count;
-                            },
-                        },
-                        toolbar: {
-                            items: [
-                                "groupPanel", // 顯示分組面板
-                                "searchPanel", // 顯示搜尋面板
-                                {
-                                    location: "after", // 在其他項目之後顯示
-                                    widget: "dxButton", // 使用 DevExtreme 按鈕
-                                    options: {
-                                        icon: "refresh", // 刷新圖示
-                                        onClick() {
-                                            fetchData(); // 點擊時重新獲取資料
-                                        },
-                                    },
-                                },
-                            ],
-                        },
-                    })
-                    .dxDataGrid("instance"); // 獲取資料表格實例
-            })
-            .catch((error) => {
-                console.error("Error fetching data:", error); // 錯誤處理
-            });
-    }
-
-    fetchData(); // 初始獲取資料
-    setInterval(fetchData, 5000); // 每 5 秒重新獲取資料
+$(async () => {
+    // 定義更新間隔選項
+    const fetchIntervalItems = [
+        {
+            text: "5秒",
+            value: 5,
+        },
+        {
+            text: "10秒",
+            value: 10,
+        },
+        {
+            text: "15秒",
+            value: 15,
+        },
+        {
+            text: "30秒",
+            value: 30,
+        },
+        {
+            text: "1分",
+            value: 60,
+        },
+    ];
 
     function setCellTemplate(element, options) {
         // 設定單元格模板，顯示角色名稱和圖片
@@ -112,4 +39,187 @@ $(() => {
             .css("display", "flex") // 設定顯示為彈性盒子
             .css("align-items", "center"); // 垂直置中對齊
     }
+
+    // 獲取網格容器的摘要資訊
+    function getGridContainerSummary(chattersCount) {
+        return {
+            groupItems: [
+                {
+                    column: "role", // 角色欄位
+                    summaryType: "count", // 計算類型為計數
+                },
+            ],
+            totalItems: [
+                {
+                    name: "ChattersCount", // 總計名稱
+                    showInColumn: "name", // 顯示在名稱欄位
+                    displayFormat: "聊天室人數: {0}", // 顯示格式
+                    summaryType: "custom", // 自定義計算類型
+                },
+            ],
+            // 自定義摘要計算
+            calculateCustomSummary: (options) => {
+                options.totalValue = chattersCount; // 設定總計值為聊天室人數
+            },
+        };
+    }
+
+    // 異步函數：獲取資料來源
+    async function fetchDataSource() {
+        let fetchedDataSource;
+
+        // 發送請求以獲取聊天室資料
+        await fetch(`/api/chatters/${channel}`)
+            .then((response) => response.json()) // 將回應轉換為 JSON 格式
+            .then((data) => {
+                fetchedDataSource = data; // 儲存獲取的資料
+            })
+            .catch((error) => {
+                console.error("獲取資料時發生錯誤:", error); // 錯誤處理
+            });
+
+        return fetchedDataSource; // 返回獲取的資料
+    }
+
+    // 異步函數：更新資料網格
+    async function updateDataGrid() {
+        let fetchedDataSource = await fetchDataSource(); // 獲取資料來源
+
+        gridContainer.option("dataSource", fetchedDataSource.chatters); // 更新網格資料來源
+        gridContainer.option(
+            "summary",
+            getGridContainerSummary(fetchedDataSource.count) // 更新摘要資訊
+        );
+    }
+
+    // 更新獲取間隔的函數
+    function updateFetchInterval() {
+        clearInterval(fetchIntervalId); // 清除現有的計時器
+        fetchInterval =
+            $("#fetchInterval").dxSelectBox("option", "value") * 1000; // 獲取新的間隔時間（毫秒）
+        fetchIntervalId = setInterval(updateDataGrid, fetchInterval); // 設定新的計時器
+
+        updateDataGrid(); // 立即更新資料網格
+    }
+
+    // 獲取初始資料來源
+    const dataSource = await fetchDataSource();
+
+    // 初始化資料網格
+    const gridContainer = $("#gridContainer")
+        .dxDataGrid({
+            dataSource: dataSource.chatters, // 設定資料來源
+            allowColumnReordering: true, // 允許重新排序欄位
+            width: "100%", // 設定寬度為 100%
+            height: "100%",
+            showBorders: true, // 顯示邊框
+            hoverStateEnabled: true, // 啟用懸停狀態
+            grouping: {
+                autoExpandAll: true, // 自動展開所有分組
+                expandMode: "rowClick", // 點擊行展開
+            },
+            searchPanel: {
+                visible: true, // 顯示搜尋面板
+            },
+            paging: {
+                enabled: false,
+            },
+            groupPanel: {
+                visible: true, // 顯示分組面板
+            },
+            selection: {
+                mode: "single", // 單選模式
+            },
+            columns: [
+                {
+                    dataField: "role", // 角色欄位
+                    caption: "角色", // 欄位標題
+                    groupIndex: 0, // 設定為第一個分組欄位
+                    groupCellTemplate: (element, options) =>
+                        setCellTemplate(element, options), // 自定義分組單元格模板
+                    cellTemplate: (element, options) =>
+                        setCellTemplate(element, options), // 自定義單元格模板
+                },
+                {
+                    dataField: "name", // 名稱欄位
+                    caption: "名稱", // 欄位標題
+                    allowGrouping: false, // 不允許分組
+                    cellTemplate(container, options) {
+                        // 自定義單元格模板，生成超連結
+                        return $("<a>", {
+                            href: `https://www.twitch.tv/${options.value}`,
+                            target: "_blank", // 在新標籤頁中打開
+                        }).text(options.value);
+                    },
+                },
+            ],
+            sortByGroupSummaryInfo: [
+                {
+                    summaryItem: "count",
+                },
+            ], // 根據分組摘要資訊排序
+            summary: getGridContainerSummary(dataSource.count),
+            toolbar: {
+                items: [
+                    {
+                        location: "before",
+                        widget: "dxSwitch", // 使用 DevExtreme 開關
+                        options: {
+                            value: true,
+                            onValueChanged(data) {
+                                $("#fetchInterval").dxSelectBox(
+                                    "option",
+                                    "disabled",
+                                    !data.value
+                                );
+
+                                if (data.value) {
+                                    updateFetchInterval();
+                                } else {
+                                    clearInterval(fetchIntervalId);
+                                }
+                            },
+                        },
+                    },
+                    {
+                        location: "before",
+                        widget: "dxSelectBox", // 使用 DevExtreme 下拉式選單
+                        options: {
+                            items: fetchIntervalItems,
+                            value: fetchIntervalItems[0].value,
+                            displayExpr: "text",
+                            valueExpr: "value",
+                            elementAttr: {
+                                id: "fetchInterval",
+                            },
+                            placeholder: "更新間隔時間",
+                            onValueChanged(data) {
+                                updateFetchInterval();
+                            },
+                        },
+                    },
+                    {
+                        name: "groupPanel",
+                        location: "after",
+                    },
+                    "searchPanel", // 顯示搜尋面板
+                    {
+                        location: "after", // 在其他項目之後顯示
+                        widget: "dxButton", // 使用 DevExtreme 按鈕
+                        options: {
+                            icon: "refresh", // 刷新圖示
+                            onClick() {
+                                updateDataGrid(); // 點擊時重新獲取資料
+                            },
+                        },
+                    },
+                ],
+            },
+        })
+        .dxDataGrid("instance"); // 獲取資料網格實例
+
+    // 設定初始獲取間隔為第一個選項的值（毫秒）
+    let fetchInterval = fetchIntervalItems[0].value * 1000;
+    // 設定定時器，定期更新資料網格
+    let fetchIntervalId = setInterval(updateDataGrid, fetchInterval);
 });
